935a8f96b325aea0c1d8ac71f6dd2ec4e6add548
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-10-18 20:25:28 +0600

    Add html and stylized css js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css"
+const form = document.forms[0];
+const scoreList = document.querySelector('.score-list');
+const refreshBtn = document.querySelector('.btn1');
+const message = document.querySelector('.btn2');
