749b199ddb47ec2a3331511e98d8019bd00684a7
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-10-19 09:14:34 +0600

    Setup leaderBoard js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import "./style.css"
-const form = document.forms[0];
-const scoreList = document.querySelector('.score-list');
-const refreshBtn = document.querySelector('.btn1');
-const message = document.querySelector('.btn2');
+import './style.css';
+import displayList from './modules/displaylist.js';
+import LeaderBoard from './modules/board.js';
+
+const leaderArr = [{ user: 'Amre', score: 465 },
+  { user: 'Nurka', score: 360 }];
+
+const scoreInput = document.querySelector('#score');
+const nameInput = document.querySelector('#name');
+const refreshBtn = document.querySelector('.refresh');
+const submitBtn = document.querySelector('.submit');
+const leaderForm = document.querySelector('.second-part');
+
+refreshBtn.addEventListener('click', () => {
+  displayList();
+});
+
+submitBtn.addEventListener('submit', (e) => {
+  e.preventDefault();
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+leaderForm.addEventListener('submit', () => {
+  const leader = new LeaderBoard(nameInput.value, scoreInput.value);
+  nameInput.value = '';
+  scoreInput.value = '';
+  leaderArr.push(leader);
+  displayList(leaderArr);
+});
+displayList(leaderArr);
