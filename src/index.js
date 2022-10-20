fd7ef11bacd4b9da22e7707cebabf4bc61354a46
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-10-20 16:08:19 +0600

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
-import displayList from './modules/displaylist.js';
+import displayList, { getValue } from './modules/displaylist.js';
 import postValue from './modules/post.js';
-import { getValue } from './modules/displaylist.js';
 
 const apiKey = 'Cw1HUhz2DRmZdE3JLvOM';
 const gameAPI = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores/`;
